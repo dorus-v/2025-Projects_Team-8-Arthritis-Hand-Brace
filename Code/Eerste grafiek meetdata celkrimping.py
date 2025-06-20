fa8658f20e24b3dad6a8c07370dc81df30874cc3
dorusv <dorus.f.vrolijk@gmail.com>
2025-06-20 14:58:06 +0200

    Figure 1

--- Code/Eerste grafiek meetdata celkrimping.py
+++ Code/Eerste grafiek meetdata celkrimping.py
@@ -3,18 +3,30 @@ import matplotlib.pyplot as plt
 
 # Gegeven data
 x_waarden = [2, 3, 4, 5, 6, 7, 8, 9]
-y_waarden = [0.6845803764097439, 0.7107355256748643, 0.7055512240760954,
-             0.7147620673723962, 0.7469254962938094, 0.7585170005156745,
-             0.7756403691946019, 0.8163512637862057]
+y_waarden = [
+            0.6738163147980806,
+            0.6974220979862596,
+            0.688413993731318, 
+            0.6946060678505313,
+            0.7258131054104111, 
+            0.7352160093373624,
+            0.7509882011038868, 
+            0.7936531053777592]
 
+theo_y_waarden = []
+while len(theo_y_waarden) < 8:
+    theo_y_waarden.append(2/3.14)
 # Plot maken
 plt.figure(figsize=(8, 5))
-plt.plot(x_waarden, y_waarden, marker='o', linestyle='-', color='blue', label='Waarden')
-plt.xlabel("Number of cells")
-plt.ylabel("Shrinkage factor")
+plt.plot(x_waarden, y_waarden, marker='o', linestyle='-', color='blue', label='Measured value')
+plt.plot(x_waarden, theo_y_waarden, linestyle='--', color='red', label='Theoretical value')
+plt.xlabel("Number of cells", fontsize=15)
+plt.ylabel("Shrinkage factor", fontsize=15)
+plt.tick_params(axis="both", labelsize=13)
 plt.xlim(2,9)
-plt.ylim(0.65, 0.85)
+plt.ylim(0.60, 0.8)
 plt.grid(True)
 plt.tight_layout()
+plt.legend()
 plt.show()
 plt.savefig("grafiek.png")
